1012e92fc4546665098ee9b1579f2c5045dfb633
Trix <dtwatson443@gmail.com>
2023-10-15 16:59:52 -0500

    Fix issues with flags

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,6 @@ async function run() {
 	console.log(`##[add-matcher]${join(__dirname, '..', '.github', 'tsc.json')}`);
 	const args = [
 		`${join(process.cwd(), 'node_modules/.bin', executable)}`,
-		'--noEmit',
-		'--noErrorTruncation',
-		'--pretty',
-		'false',
-		'--incremental',
-		'false',
-	];
-	if (project) {
-		args.push('--project', project);
-	}
-	if (build) {
-		args.splice(1, 0, '--build', build);
-		// Remove --noEmit and --noErrorTruncation, which are unsupported with --build
-		args.splice(3, 2);
-		// Change --incremental false for --incremental true, as incremental builds are required for composite builds
-		args.splice(-1, 1, 'true');
-	}
 	try {
 		await exec('node', args);
 	} catch (error) {
