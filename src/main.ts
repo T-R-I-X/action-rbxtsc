695b06dae2ff43d4927c8b017d0180f942cc6789
Trix <dtwatson443@gmail.com>
2023-10-15 17:04:56 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,9 @@ import { exec } from '@actions/exec';
 import { join } from 'path';
 
 async function run() {
-	const project = getInput('project');
-	const build = getInput('build');
 	const executable = getInput('executable');
 	console.log(`##[add-matcher]${join(__dirname, '..', '.github', 'tsc.json')}`);
-	const args = [
-		`${join(process.cwd(), 'node_modules/.bin', executable)}`,
+	const args = [`${join(process.cwd(), 'node_modules/.bin', executable)}`]
 	try {
 		await exec('node', args);
 	} catch (error) {
